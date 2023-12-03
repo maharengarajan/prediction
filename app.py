1ca4f805833be1e17e466d762adafd3ce146c612
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-12-03 20:33:09 +0530

    predicted in webapp&works well

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pickle
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, url_for
 import numpy as np
 import pandas as pd
 
@@ -8,9 +8,15 @@ app = Flask(__name__)
 # load model
 model = pickle.load(open("model.pkl", "rb"))
 
-# this API is resposible for collect data & do prediction
-@app.route('/predict_api', methods=['POST'])
-def predict_api():
+
+@app.route('/')
+def home():
+    #return 'Hello World'
+    return render_template('home.html')
+
+# this API is resposible for collect data & do prediction in postman
+@app.route('/predict_postman', methods=['POST'])
+def predict_postman():
     # collect data in json format
     data = request.json['data']
     print(data)
@@ -19,6 +25,21 @@ def predict_api():
     # prediction
     output = model.predict(new_data)[0]
     return jsonify(output)
+
+
+# this API is resposible for collect data & do prediction in webapp
+@app.route('/predictWebapp', methods=['POST'])
+def predictWebapp():
+    # collect values from webapp
+    data = [float(x) for x in request.form.values()]
+    # convert collected values into 2D array
+    final_features = [np.array(data)]
+    print(data)
+    # prediction
+    output=model.predict(final_features)[0]
+    print(output)
+    #output = round(prediction[0], 2)
+    return render_template('home.html', prediction_text="Airfoil pressure is  {}".format(output))
 
 
 if __name__=="__main__":
